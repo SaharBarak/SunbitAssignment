ba721ebf55614668fcbbe3fc2fba1c4af3a94bf6
Sahar Barak <sahar.h.barak@gmail.com>
2021-10-10 11:39:13 +0300

    base

---
+++ src/main/java/customerapi/sunbitassignment/SunbitAssignmentApplication.java
@@ -0,0 +1,13 @@
+package customerapi.sunbitassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbitAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunbitAssignmentApplication.class, args);
+    }
+
+}
